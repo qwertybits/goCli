9e7dbcbbbd0d9eace92d0ac1ce0ab58c064da06f
Ivan Kotkov <ivankot.dvk@gmail.com>
2025-07-22 22:08:55 +0300

    added basic cli user interaction

--- task/tasktracker.go
+++ task/tasktracker.go
@@ -5,23 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strconv"
 )
-
-type CLICommand struct {
-	cmd       string
-	arguments []string
-}
-
-var aviableCommands = map[string]bool{
-	"add":              true,
-	"update":           true,
-	"delete":           true,
-	"mark-in-progress": true,
-	"mark-done":        true,
-	"list":             true,
-}
-
-const defaultJsonPath = "task.json"
 
 func loadTasksFromJson(path string) ([]TaskObj, error) {
 	var result = make([]TaskObj, 0)
@@ -48,40 +33,69 @@ func exportTasksToJson(path string, storage []TaskObj) error {
 	return nil
 }
 
-func addTask(content string, storage *[]TaskObj) {
-	*storage = append(*storage, NewTask(len(*storage), content))
+func addTask(input CLICommand, storage *[]TaskObj) error {
+	if !valideArgumentCount(&input, 1) {
+		return errors.New("addTask: not enough arguments")
+	}
+	content := input.arguments[0]
+	id := len(*storage)
+	*storage = append(*storage, NewTask(id, content))
+	return nil
 }
 
-func printTasks(storage *[]TaskObj) {
+func printTasks(input CLICommand, storage *[]TaskObj) error {
 	for _, task := range *storage {
 		fmt.Printf("%v\n", task)
 	}
+	return nil
 }
 
-func updateStatus(id int, new string, storage *[]TaskObj) error {
+func updateStatus(input CLICommand, storage *[]TaskObj) error {
+
+	if !valideArgumentCount(&input, 2) {
+		return errors.New("updateStatus: not enough arguments")
+	}
+
+	id, err := strconv.Atoi(input.arguments[1])
+	new := input.arguments[0]
+	if err != nil {
+		return errors.New("updateStatus: invalid id")
+	}
 	if !validateId(id, storage) {
 		return errors.New("id range out")
 	}
 	return (*storage)[id].SetStatus(new)
 }
 
-func validateId(id int, storage *[]TaskObj) bool {
-	return !(id >= len(*storage) || id < 0)
-}
+func updateDescription(input CLICommand, storage *[]TaskObj) error {
 
-func updateDescription(id int, new string, storage *[]TaskObj) error {
+	if !valideArgumentCount(&input, 2) {
+		return errors.New("updateDescription: not enough arguments")
+	}
+
+	id, err := strconv.Atoi(input.arguments[1])
+	new := input.arguments[0]
+
+	if err != nil {
+		return errors.New("updateDescription: invalid id")
+	}
 	if !validateId(id, storage) {
 		return errors.New("id range out")
 	}
 	return (*storage)[id].SetDescription(new)
 }
 
-func deleteTaskById(id int, storage *[]TaskObj) error {
-
+func deleteTaskById(input CLICommand, storage *[]TaskObj) error {
+	if !valideArgumentCount(&input, 1) {
+		return errors.New("deleteTask: not enough arguments")
+	}
+	id, err := strconv.Atoi(input.arguments[0])
+	if err != nil {
+		return errors.New("deleteTask: invalid id")
+	}
 	if !validateId(id, storage) {
 		return errors.New("id range out")
 	}
-
 	for i := id; i < len(*storage)-1; i++ {
 		(*storage)[i] = (*storage)[i+1]
 		(*storage)[i].SetId(i)
@@ -106,12 +120,38 @@ func valideArgumentCount(input *CLICommand, need int) bool {
 	return len(input.arguments) >= need
 }
 
+func validateId(id int, storage *[]TaskObj) bool {
+	return !(id >= len(*storage) || id < 0)
+}
+
+var availableCommands = map[string]func(CLICommand, *[]TaskObj) error{
+	"add":              addTask,
+	"update":           updateDescription,
+	"delete":           deleteTaskById,
+	"mark-in-progress": updateStatus,
+	"mark-done":        updateStatus,
+	"mark-todo":        updateStatus,
+	"list":             printTasks,
+}
+
+type CLICommand struct {
+	cmd       string
+	arguments []string
+}
+
+const defaultJsonPath = "task.json"
+
 func Run() {
-	var tasks, err = loadTasksFromJson(defaultJsonPath)
+	tasks, err := loadTasksFromJson(defaultJsonPath)
 	if err != nil {
 		fmt.Printf("loadTaskFromJson: %v", err)
 		return
 	}
+	defer func() {
+		if err := exportTasksToJson(defaultJsonPath, tasks); err != nil {
+			fmt.Printf("%v", err)
+		}
+	}()
 
 	command, err := getCommand()
 	if err != nil {
@@ -119,10 +159,8 @@ func Run() {
 		return
 	}
 
-	value := aviableCommands[command.cmd]
-	fmt.Printf("%v\n%v", value, command)
-
-	if err := exportTasksToJson(defaultJsonPath, tasks); err != nil {
+	run := availableCommands[command.cmd]
+	if err := run(command, &tasks); err != nil {
 		fmt.Printf("%v", err)
 	}
 }
